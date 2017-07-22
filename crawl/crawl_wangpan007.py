bd49a8a35fbb77c60cf62d979c3581a5590df18d
wycheng <wychengpublic@163.com>
2017-07-23 00:01:13 +0800

    调整爬取资源的模块之间的协作关系

--- crawl/crawl_wangpan007.py
+++ crawl/crawl_wangpan007.py
@@ -4,9 +4,7 @@ import requests
 import json
 from lxml import html
 import time
-import os
-import codecs
-import zipfile
+
 
 def search(src_name,page):
     url_api = "https://m.wangpan007.com/search/ajax?keyword="+src_name+"&page="+str(page)
@@ -17,6 +15,7 @@ def search(src_name,page):
         print "failure...request again"
         return search(src_name,page)
 
+# 爬取所有资源，返回五元组list
 def search_all(src_name):
     jsob = search(src_name,1)
     pageCount = jsob["pageCount"]
@@ -24,6 +23,8 @@ def search_all(src_name):
     if pageCount == 0 :
         return None
     else:
+        cache = []  # 用来暂时存放资源数据，以待进一步处理
+
         for p in range(1,pageCount+1):
             jsob = search(src_name,p)
             src_list = jsob["list"]
@@ -43,16 +44,20 @@ def search_all(src_name):
                 feed_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(feed_time/1000))
                 suffix = src["suffix"][1:]
 
-                if not os.path.exists(u"../text/" + src_name + u"_资源链接文档合集" + u"/" + share_cat):
-                    os.makedirs(u"../text/" + src_name + u"_资源链接文档合集" + u"/" + share_cat)
+                info =str(feed_time) + u"  " + str(view_cnt) + u"次浏览/" + str(download_cnt) + u"次下载/" + str(transfer_cnt) + u"次转存"
+                URL = share_url
+                heat = 0.3*view_cnt + 0.4*download_cnt + 0.3*transfer_cnt
+                cache.append({'name': filename, 'url': URL, 'cat': suffix, 'info': info, 'heat': heat})# 加进缓存list
 
-                file =codecs.open(u"../text/" + src_name + u"_资源链接文档合集" + u"/" + share_cat + u"/" + u"资源种类:" + suffix + u".txt", 'a+',"utf-8")
-                file.write(u"资源编号: " + str(src_cnt) + "\n")
-                file.write(u"资源名称: \n" + filename + u"\n\n")
-                file.write(u"相关信息: \n" + str(feed_time) + u"  " + str(view_cnt) + u"次浏览/" + str(download_cnt) + u"次下载/" + str(transfer_cnt) + u"次转存\n\n")
-                file.write(u"链接地址: \n" + share_url + u"\n")
-                file.write("---------------------------------\n\n")
-                
+                # if not os.path.exists(u"../text/" + src_name + u"_资源链接文档合集" + u"/" + share_cat):
+                #     os.makedirs(u"../text/" + src_name + u"_资源链接文档合集" + u"/" + share_cat)
+                #
+                # file =codecs.open(u"../text/" + src_name + u"_资源链接文档合集" + u"/" + share_cat + u"/" + suffix + u".txt", 'a+',"utf-8")
+                # file.write(u"资源编号: " + str(src_cnt) + "\n")
+                # file.write(u"资源名称: \n" + filename + u"\n\n")
+                # file.write(u"链接地址: \n" + share_url + u"\n")
+                # file.write("---------------------------------\n\n")
+                #
                 print str(src_cnt) + "-----------------------------"
                 print u"资源名称" + filename
                 print u"链接" + share_url
@@ -65,22 +70,8 @@ def search_all(src_name):
                 print str(feed_time)
                 print "---------------------------------\n"
 
-        make_zip(u"../text/" + src_name + u"_资源链接文档合集",u"../text/" + src_name + u"_资源链接文档合集.zip")
+        # make_zip(u"../text/" + src_name + u"_资源链接文档合集",u"../text/" + src_name + u"_资源链接文档合集.zip")
 
-# 打包目录为zip文件（未压缩）
-def make_zip(source_dir, output_filename):
-    zipf = zipfile.ZipFile(output_filename, 'w')
-    pre_len = len(os.path.dirname(source_dir))
-    for parent, dirnames, filenames in os.walk(source_dir):
-        for filename in filenames:
-            pathfile = os.path.join(parent, filename)
-            arcname = pathfile[pre_len:].strip(os.path.sep)  # 相对路径
-            zipf.write(pathfile, arcname)
-    zipf.close()
-                
+        return cache
 
-
-# try:
 search_all(u"欢乐颂")
-# except:
-#     print "something wrong!"
